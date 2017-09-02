8126fee12c7b739cb47910150df110e760898442
heartstchr <heartstchr@live.com>
2017-09-02 16:59:05 +0530

    [workers]

--- workers/index.js
+++ workers/index.js
@@ -11,23 +11,23 @@ function getWorkers(usr,pass,option,callback){
     if(!option){
         return callback('Option not Defined',null);
     }
-    if(!option.os){return callback("Os is not defined",null)}
-    if(!option.os_version){return callback("Os Version is not defined",null)}
-    if(!option.browser){return callback("Browser is not defined",null)}
-    if(!option.device){return callback("Device is not defined",null)}
-    if(!option.browser_version){return callback("Browser Version is not defined",null)}
+    if(option.os ==='undefined'){return callback("Os is not defined",null)}
+    if(option.os_version==='undefined'){return callback("Os Version is not defined",null)}
+    if(option.browser==='undefined'){return callback("Browser is not defined",null)}
+    if(option.device==='undefined'){return callback("Device is not defined",null)}
+    if(option.browser_version==='undefined'){return callback("Browser Version is not defined",null)}
     var query='';
     var total = Object.keys(option).length;
     Object.keys(option).forEach(function (ele,index) {
         if(index !== total-1){
-            query += ele + '= "'+ option[ele] +'"&&'
+            query += ele + '='+ option[ele] +'&'
         }else{
-            query += ele + '="'+ option[ele] + '"'
+            query += ele + '='+ option[ele]
         }
     });
     console.log(query);
     var baseAuth= '"'+usr+':'+pass +'"';
-    var cmd ='curl -X POST -u '+baseAuth+ baseUrl +'/workers?'+query;
+    var cmd ='curl -X POST -u '+baseAuth+ ' -sS '+baseUrl +'/workers?'+query;
     console.log(cmd);
     exec(cmd, function(err, out, code) {
         if (err instanceof Error)
